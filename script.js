c4342f1a720fcb70d6d761a7a188458ade0b0bf1
Anwar <anwarhusengit@gmail.com>
2024-11-07 00:12:33 +0530

    appearance change

--- script.js
+++ script.js
@@ -1,113 +1,123 @@
-let body = document.body;
-let playerScore = document.querySelector("#player-score");
-let computerScore = document.querySelector("#computer-score");
-let btn = document.querySelectorAll("button");
-let choosing = document.querySelector("#choosing");
-let rock = document.querySelector("#rock");
-let paper = document.querySelector("#paper");
-let scissor = document.querySelector("#scissor");
-let Crock = document.querySelector("#Crock");
-let Cpaper = document.querySelector("#Cpaper");
-let Cscissors = document.querySelector("#Cscissor");
-let reset = document.querySelector("#reset");
-let score1 = 0;
-let score2 = 0;
+let gameContent = document.querySelector(".game-content");
+let h1 = document.querySelector("h1");
+let h3 = document.querySelector("h3");
+let rock = document.querySelector(".rock");
+let paper = document.querySelector(".paper");
+let scissor = document.querySelector(".scissor");
+let win = document.querySelector("#win");
+let lost = document.querySelector("#lost");
+let draw = document.querySelector("#draw");
+let replay = document.querySelector("#replay");
+let rightImg = document.querySelector(".right-img");
+let leftImg = document.querySelector(".left-img");
+let h3display = document.querySelector(".h3display");
+let winSound = document.getElementById("win-sound");
+let lossSound = document.getElementById("loss-sound");
+let drawSound = document.getElementById("draw-sound");
+let winCount, lostCount, drawCount;
+
+winCount = 0;
+lostCount = 0;
+drawCount = 0;
 
 rock.addEventListener("click", () => {
-  navigator.vibrate(500);
-  rock.style.border = "2px solid blue";
-  choosing.innerText = "Computer is choosing...";
-  let computer = Math.floor(Math.random() * 3);
-  btn.forEach((b) => {
-    b.disabled = true;
-  });
-  setTimeout(() => {
-    if (computer === 0) {
-      Crock.style.border = "2px solid red";
-      choosing.innerText = "It's a Draw! (Computer chose Rock!)";
-    } else if (computer === 1) {
-      score2 += 1;
-      computerScore.innerText = score2;
-      Cpaper.style.border = "2px solid red";
-      choosing.innerText = "Computer Won! (Computer chose Paper!)";
-    } else {
-      score1 += 1;
-      playerScore.innerText = score1;
-      Cscissors.style.border = "2px solid red";
-      choosing.innerText = "You Won! (Computer chose Scissor!)";
-    }
-    navigator.vibrate(500);
-  }, 1000);
+  navigator.vibrate(100);
+  playGame("rock");
 });
-
 paper.addEventListener("click", () => {
-  navigator.vibrate(500);
-  btn.forEach((b) => {
-    b.disabled = true;
-  });
-  paper.style.border = "2px solid blue";
-  choosing.innerText = "Computer is choosing...";
-  let computer = Math.floor(Math.random() * 3);
-
-  setTimeout(() => {
-    if (computer === 0) {
-      score1 += 1;
-      playerScore.innerText = score1;
-      Crock.style.border = "2px solid red";
-      choosing.innerText = "You Won! (Computer chose Rock!)";
-    } else if (computer === 1) {
-      Cpaper.style.border = "2px solid red";
-      choosing.innerText = "It's a Draw! (Computer chose Paper!)";
-    } else {
-      score2 += 1;
-      computerScore.innerText = score2;
-      Cscissors.style.border = "2px solid red";
-      choosing.innerText = "Computer Won! (Computer chose Scissor!)";
-    }
-    navigator.vibrate(500);
-  }, 1000);
+  navigator.vibrate(100);
+  playGame("paper");
 });
-
 scissor.addEventListener("click", () => {
-  navigator.vibrate(500);
-  btn.forEach((b) => {
-    b.disabled = true;
-  });
-  scissor.style.border = "2px solid blue";
-  choosing.innerText = "Computer is choosing...";
-  let computer = Math.floor(Math.random() * 3);
-
-  setTimeout(() => {
-    if (computer === 0) {
-      score2 += 1;
-      computerScore.innerText = score2;
-      Crock.style.border = "2px solid red";
-      choosing.innerText = "Computer Won! (Computer chose Rock!)";
-    } else if (computer === 1) {
-      score1 += 1;
-      playerScore.innerText = score1;
-      Cpaper.style.border = "2px solid red";
-      choosing.innerText = "You Won! (Computer chose Paper!)";
-    } else {
-      Cscissors.style.border = "2px solid red";
-      choosing.innerText = "It's a Draw! (Computer chose Scissor!)";
-    }
-    navigator.vibrate(500);
-  }, 1000);
+  navigator.vibrate(100);
+  playGame("scissor");
 });
 
-reset.addEventListener("click", () => {
-  btn.forEach((b) => {
-    b.disabled = false;
-  });
-  Crock.style.border = "none";
-  Cpaper.style.border = "none";
-  Cscissors.style.border = "none";
-  rock.style.border = "none";
-  paper.style.border = "none";
-  scissor.style.border = "none";
-  choosing.innerText = "";
+function playGame(playerChoice) {
+  h3display.style.display = "block";
+  h3display.innerText = "";
 
-  playerScore.innerText = score1;
-  computerScore.innerText = score2;
+  gameContent.style.display = "none";
+  rightImg.style.display = "flex";
+  leftImg.style.display = "flex";
+
+  let computerChoice = Math.floor(Math.random() * 3);
+  let choices = ["rock", "paper", "scissor"];
+  let computerChoiceName = choices[computerChoice];
+
+  leftImg.src = `/images/${playerChoice}-left.png`;
+  rightImg.src = `/images/${computerChoiceName}-right.png`;
+
+  leftImg.style.animation = "left-entry 1s forwards, head-turn 1s forwards 2s";
+  rightImg.style.animation =
+    "right-entry 1s forwards, head-turn 1s forwards 2s";
+
+  setTimeout(() => {
+    if (playerChoice === computerChoiceName) {
+      h3.innerText = "Draw!";
+      rightImg.style.display = "none";
+      leftImg.style.display = "none";
+      drawCount++;
+      draw.innerText = drawCount;
+      drawSound.play();
+    } else if (
+      (playerChoice === "rock" && computerChoiceName === "scissor") ||
+      (playerChoice === "paper" && computerChoiceName === "rock") ||
+      (playerChoice === "scissor" && computerChoiceName === "paper")
+    ) {
+      h3.innerText = "You Won!";
+      winCount++;
+      win.innerText = winCount;
+      rightImg.style.display = "none";
+      leftImg.style.display = "none";
+      winSound.play();
+      createStars();
+    } else {
+      h3.innerText = "You Lost!";
+      lostCount++;
+      lost.innerText = lostCount;
+      rightImg.style.display = "none";
+      leftImg.style.display = "none";
+      lossSound.play();
+    }
+    replay.style.display = "block";
+  }, 1500);
+  h3display.style.display = "flex";
+  h3display.style.justifyContent = "center";
+  h3display.style.alignItems = "center";
+  h3display.style.height = "100vh";
+  h3display.style.margin = "0";
+}
+
+function createStars() {
+  for (let i = 0; i < 40; i++) {
+    let star = document.createElement("div");
+    star.classList.add("star");
+    star.style.left = `${Math.random() * 100}%`;
+    star.style.top = `${Math.random() * 100}%`;
+    star.style.animationDuration = `${Math.random() * 2 + 3}s`;
+    star.style.animationDelay = `${Math.random() * 2}s`;
+    document.getElementById("stars-container").appendChild(star);
+  }
+}
+
+function clearStars() {
+  const starsContainer = document.getElementById("stars-container");
+  while (starsContainer.firstChild) {
+    starsContainer.removeChild(starsContainer.firstChild);
+  }
+}
+
+replay.addEventListener("click", () => {
+  gameContent.style.display = "flex";
+  gameContent.style.flexDirection = "column";
+  gameContent.style.justifyContent = "center";
+  gameContent.style.alignItems = "center";
+  gameContent.style.height = "100vh";
+
+  rightImg.style.display = "none";
+  leftImg.style.display = "none";
+  replay.style.display = "none";
+  h3display.style.display = "none";
+  clearStars();
 });
